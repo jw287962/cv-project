b39ee7028ebfa157a77edd0eae6f85531e7a5390
JasonPC <jasonwong287962@gmail.com>
2023-02-11 01:25:12 -0600

    can add new experiences with form no edit yet

--- src/component/Experience.js
+++ src/component/Experience.js
@@ -6,13 +6,14 @@ class Experience extends React.Component{
       super(props);
       
         this.state ={
-          data: this.props.data,
+          currentForm: {title: "", company: "", description: ""},
+          data: [],
           button: true,
           noUpdate: props.noUpdate,
+
+
         }
-
-        console.log(this.state.data)
-
+          console.log(this.state.data);
       this.editExperience = (e) => {
         this.setState({
           button: !this.state.button,
@@ -22,13 +23,52 @@ class Experience extends React.Component{
       }
 
     this.saveExperience = (e) => {
+      e.preventDefault();
+      console.log(this.state.currentForm);
         this.setState({
+          data: this.state.data.concat(this.state.currentForm),
+          currentForm: {title: "", company: "", description: ""},
           button: !this.state.button,
         })
     }
 
     this.submitFormExperiences = (e) => {
       e.preventDefault();
+      this.setState()
+    }
+
+    this.reEditExperience = (e) => {
+      let arrayNum;
+      if(e.target.className === 'workexperiences'){
+      arrayNum = e.target.id;
+      }else{
+        arrayNum = e.target.parentElement.id;
+      }
+
+      console.log(arrayNum);
+
+      e.stopPropagation()
+      e.preventDefault();
+
+    }
+
+    this.formOnChange = (e) => {
+      console.log(e.target.id);
+      if(e.target.id === 'company'){
+        this.setState({
+          currentForm: { title: this.state.currentForm.title,  description: this.state.currentForm.description, company: e.target.value}
+        })
+      }
+      else if(e.target.id === 'title'){
+        this.setState({
+          currentForm: {company: this.state.currentForm.company,  description: this.state.currentForm.description, title: e.target.value}
+        })
+      }      
+      else if(e.target.id === 'description'){
+        this.setState({
+          currentForm: {company: this.state.currentForm.company,  title: this.state.currentForm.title, description: e.target.value}
+        })
+      }
     }
     }
 
@@ -54,11 +94,11 @@ class Experience extends React.Component{
                   <div className={(this.state.button) ? "hidden" : "nothidden"}>
                     <form className="expform" onSubmit={this.submitFormExperiences}>
                         <label htmlFor="company">Company Name</label>
-                        <input type='text' id="company" name="company"></input>
-                        <label  htmlFor="title">Position Title</label>
-                        <input type='text' name="title"></input>
+                        <input type='text' id="company" name="company" value={this.state.currentForm.company} onChange={this.formOnChange}></input>
+                        <label  htmlFor="title">Position Title </label>
+                        <input type='text' name="title" id="title" value={this.state.currentForm.title}  onChange={this.formOnChange}></input>
                         <label htmlFor="description">Description</label>
-                        <textarea id="description"></textarea>
+                        <textarea id="description" value={this.state.currentForm.description}  onChange={this.formOnChange}></textarea>
                         <label htmlFor="start">Start Date</label>
                         <input type='date' id="start" name="start"></input>
                         <label htmlFor="end">End Date</label>
@@ -70,7 +110,7 @@ class Experience extends React.Component{
                   {this.state.data.map((ele,i) => {
                     if(ele.title === "") return;
                         return(
-                          <div className={'workexperiences'} id={i}key={i}>
+                          <div className={'workexperiences'} id={i}key={i} onClick={this.reEditExperience}>
                               <div className="title">Title: {ele.title}</div>
                               <div className="company"> {ele.company}</div>
 
